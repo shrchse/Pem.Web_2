d8b86f94508a6ced7d5c340d619d46a58086cd22
Oling Anugrah <92003596+shrchse@users.noreply.github.com>
2022-05-30 10:56:05 +0800

    Update Login.php

--- PRAK-6/Login.php
+++ PRAK-6/Login.php
@@ -18,27 +18,5 @@ if(isset($_POST['login'])){
             header("refresh:5; url=FormLogin.php");    
         }
 }
-
-
-    // session_start();
-    // include 'Koneksi.php';
-    // $member = $_POST['nomor_member'];
-    // $pass = $_POST['password'];
-
-    // $command = connect()->prepare ("SELECT * FROM member WHERE nomor_member = :member");
-    // $command -> bindParam('Member', $member, PDO::PARAM_STR);
-    // $command -> execute();
-
-    // $data = $command->fetch();
-    // if($data){
-    //     if(password_verify($pass, $data["password"])){
-    //         $_SESSION["nomor_member"] = $data["member"];
-    //         $_SESSION["nama_member"] = $hasil['nama'];
-    //         header("Location: Main.php");  
-    //     } else echo "test";
-    // } else {
-    //     echo "Nomor Member atau Password yang Anda masukan salah";
-    //     header("refresh:3; url=FormLogin.php");
-    // }
 ?>
 
